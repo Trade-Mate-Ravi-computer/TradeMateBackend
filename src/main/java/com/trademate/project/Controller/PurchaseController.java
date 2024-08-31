e7b617e8fa5f944e774f4c6d2d0ce00d64c7c3b3
Ravi Maurya <93729578+Ravi2977@users.noreply.github.com>
2024-08-31 07:38:07 +0530

    Update PurchaseController.java

--- src/main/java/com/trademate/project/Controller/PurchaseController.java
+++ src/main/java/com/trademate/project/Controller/PurchaseController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/purchase")
-@CrossOrigin(value = {"http://localhost:3000","https://trade-mate-pearl.vercel.app/","http://ec2-16-171-11-228.eu-north-1.compute.amazonaws.com:8080","http://ec2-51-20-54-251.eu-north-1.compute.amazonaws.com:3000/"})public class PurchaseController {
+@CrossOrigin(value = {"http://localhost:3000","https://trade-mate-pearl.vercel.app/","http://ec2-16-171-11-228.eu-north-1.compute.amazonaws.com:8080","http://51.20.69.152/"})public class PurchaseController {
     @Autowired
     private PurchaseService purchaseService;
 @Autowired
