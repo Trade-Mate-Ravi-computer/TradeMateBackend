e95b17988c533ac470f3c637fa149eb609497d99
Ravi Maurya <93729578+Ravi2977@users.noreply.github.com>
2024-08-31 07:39:23 +0530

    Update UserController.java

--- src/main/java/com/trademate/project/Controller/UserController.java
+++ src/main/java/com/trademate/project/Controller/UserController.java
@@ -13,7 +13,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @RestController
-@CrossOrigin(value = {"http://localhost:3000","https://trade-mate-pearl.vercel.app/","http://ec2-16-171-11-228.eu-north-1.compute.amazonaws.com:8080","http://ec2-51-20-54-251.eu-north-1.compute.amazonaws.com:3000/"})@RequestMapping("/user")
+@CrossOrigin(value = {"http://localhost:3000","https://trade-mate-pearl.vercel.app/","http://ec2-16-171-11-228.eu-north-1.compute.amazonaws.com:8080","http://51.20.69.152/"})@RequestMapping("/user")
 public class UserController {
     private UserService userService;
     @Autowired
